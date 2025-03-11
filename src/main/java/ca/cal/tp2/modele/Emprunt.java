cbed5664b81a7553a886a155fd99cde362cf1307
victoria-fp <victoria@isbconsultant.com>
2025-03-11 00:44:05 -0400

    Modification des classes modèle Emprunt et EmpruntDetail

--- src/main/java/ca/cal/tp2/modele/Emprunt.java
+++ src/main/java/ca/cal/tp2/modele/Emprunt.java
@@ -3,24 +3,32 @@ package ca.cal.tp2.modele;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Emprunt {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private LocalDate dateEmprunt;
-    private boolean statut;
+    private boolean statut = false;
 
     @ManyToOne
     @JoinColumn
     private Emprunteur emprunteur;
 
     @OneToMany(mappedBy = "emprunt", cascade = CascadeType.ALL)
-    private List<EmpruntDetail> empruntDetails;
+    private List<EmpruntDetail> empruntDetails = new ArrayList<>();
+
+    public void addEmpruntDetail(EmpruntDetail empruntDetail) {
+        empruntDetails.add(empruntDetail);
+        setEmpruntDetails(empruntDetails);
+    }
 }
--- src/main/java/ca/cal/tp2/modele/EmpruntDetail.java
+++ src/main/java/ca/cal/tp2/modele/EmpruntDetail.java
@@ -3,21 +3,28 @@ package ca.cal.tp2.modele;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Setter
 public class EmpruntDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private LocalDate dateRetourPrevue;
-    private LocalDate dateRetourActuelle;
-    private boolean statut;
+    private LocalDate dateRetourActuelle = null;
+    private boolean statut = false;
 
     @ManyToOne
     @JoinColumn
     private Emprunt emprunt;
+
+    @OneToMany
+    List<Document> documents = new ArrayList<>();
 }
